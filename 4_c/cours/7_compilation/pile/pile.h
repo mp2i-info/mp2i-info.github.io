b2c7606fd1c0554e763c8dd0d5525b6763769a67
Quentin Fortier <qpfortier@gmail.com>
2021-12-02 19:58:41 +0000

    Merge branch 'master' of https://github.com/fortierq/MP2I_src

--- 4_c/cours/7_compilation/pile/pile.h
+++ 4_c/cours/7_compilation/pile/pile.h
@@ -1,16 +1,15 @@
 #include <stdbool.h>
 
-struct pile {
-    int* t; // tableau contenant les éléments
+typedef struct
+{
+    int *t;   // tableau contenant les éléments
     int size; // nombre d'éléments (= dessus de la pile)
-};
-
-typedef struct pile pile;
+} pile;
 
 pile create(int);
 
 bool is_empty(pile);
 
-void push(pile*, int);
+void push(pile *, int);
 
-int pop(pile*);
+int pop(pile *);
