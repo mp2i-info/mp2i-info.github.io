a452f8f9f74b10b98ac526263b999bf79fcd20ee
Quentin Fortier <qpfortier@gmail.com>
2022-04-04 09:37:46 +0200

    Update test.sql

--- 9_sql/test.sql
+++ 9_sql/test.sql
@@ -4,4 +4,4 @@ INSERT INTO voitures VALUES (3,'Alpine','Renault');
 INSERT INTO voitures VALUES (4,'Golf','Volkswagen');
 INSERT INTO voitures VALUES (2,'Mégane','Renault');
 INSERT INTO voitures VALUES (5,'Fiesta','Ford');
-INSERT INTO voitures VALUES (6,'Polo','Volkswagen');
+INSERT INTO voitures VALUES (6,'Pblo','Volkswagen');
