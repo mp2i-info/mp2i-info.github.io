1d7e9839752b0402c1d3ac4ff7c1b52b95bf2b11
Quentin Fortier <qpfortier@gmail.com>
2022-04-10 15:14:15 +0200

    Delete test.sql

--- 9_sql/test.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE voitures( id integer, nom text, marque text);
-INSERT INTO voitures VALUES (1,'Kangoo','Renault');
-INSERT INTO voitures VALUES (3,'Alpine','Renault');
-INSERT INTO voitures VALUES (4,'Golf','Volkswagen');
-INSERT INTO voitures VALUES (2,'Mégane','Renault');
-INSERT INTO voitures VALUES (5,'Fiesta','Ford');
-INSERT INTO voitures VALUES (6,'Pblo','Volkswagen');
