22ac6e0c054515462599d368ba51581fd821ec02
Quentin Fortier <qpfortier@gmail.com>
2022-05-09 12:22:33 +0000

    Rm drop

--- 10_sql/pokemon/pokemon.sql
+++ 10_sql/pokemon/pokemon.sql
@@ -1,14 +1,5 @@
 
 
-
-
-
-
-DROP TABLE IF EXISTS detient_pokemons, est_type, evolue_en;
-DROP TABLE IF EXISTS attaques, types, pokemons, dresseurs;
-
-
-
 CREATE TABLE pokemons (
 	id SERIAL NOT NULL,
 	nom varchar(10) NOT NULL,
@@ -76,45 +67,6 @@ CREATE TABLE attaques (
 );
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
-
---
 
 TRUNCATE TABLE attaques CASCADE;
 TRUNCATE TABLE types CASCADE;
