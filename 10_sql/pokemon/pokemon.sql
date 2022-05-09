9d2b2ddaeae22287bf890c85d80b5735b943f6a0
Quentin Fortier <qpfortier@gmail.com>
2022-05-09 15:15:35 +0200

    Update pokemon.sql

--- 10_sql/pokemon/pokemon.sql
+++ 10_sql/pokemon/pokemon.sql
@@ -13,3 +13,49 @@ CREATE TABLE evolue_en (
 	FOREIGN KEY(pokemon_base_id) REFERENCES pokemons(id),
 	FOREIGN KEY(pokemon_evol_id) REFERENCES pokemons(id)
 );
+
+CREATE TABLE types (
+	id SERIAL NOT NULL,
+	libelle varchar(10) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
+
+CREATE TABLE est_type (
+	pokemon_id int NOT NULL,
+	type_id int NOT NULL,
+	FOREIGN KEY(pokemon_id) REFERENCES pokemons(id),
+	FOREIGN KEY(type_id) REFERENCES types(id)
+);
+	
+
+CREATE TABLE dresseurs (
+	id SERIAL NOT NULL,
+	nom varchar(25) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
+
+
+CREATE TABLE detient_pokemons (
+	dresseur_id int NOT NULL, 
+	pokemon_id int NOT NULL,  
+	niveau int NOT NULL,
+	FOREIGN KEY(dresseur_id) REFERENCES dresseurs(id),
+	FOREIGN KEY(pokemon_id) REFERENCES pokemons(id)
+);
+
+
+
+CREATE TABLE attaques (
+	id SERIAL NOT NULL,
+	libelle varchar(50) NOT NULL,
+	type_id int NOT NULL,
+	pp int NOT NULL,
+	puissance int NULL,
+	precision int NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(type_id) REFERENCES types(id)
+);
