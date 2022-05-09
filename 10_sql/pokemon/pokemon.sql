704facb010c330a911fa433d131d71fcb0050d4c
Quentin Fortier <qpfortier@gmail.com>
2022-05-09 15:15:07 +0200

    Update pokemon.sql

--- 10_sql/pokemon/pokemon.sql
+++ 10_sql/pokemon/pokemon.sql
@@ -6,3 +6,10 @@ CREATE TABLE pokemons (
 	PRIMARY KEY(id)
 );
 
+CREATE TABLE evolue_en (
+	pokemon_base_id int NOT NULL,
+	pokemon_evol_id int NOT NULL,
+	niveau int NOT NULL,
+	FOREIGN KEY(pokemon_base_id) REFERENCES pokemons(id),
+	FOREIGN KEY(pokemon_evol_id) REFERENCES pokemons(id)
+);
