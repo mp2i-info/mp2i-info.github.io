3ca6b65ecd7cb9033261c0d86cd3ec7d1c585631
Quentin Fortier <qpfortier@gmail.com>
2022-07-03 16:03:02 +0000

    Add shapes

--- graph_mp2i/graph.js
+++ graph_mp2i/graph.js
@@ -1,11 +1,11 @@
-let namespace = joint.shapes;
+const namespace = joint.shapes;
 
-let graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+const graph = new joint.dia.Graph({}, { cellNamespace: namespace });
 
 new joint.dia.Paper({
     el: document.getElementById('graph'),
     model: graph,
-    width: 600,
+    width: 800,
     height: 400,
     // (to ensure that opening the link is not prevented on touch devices)
     elementView: joint.dia.ElementView.extend({
@@ -18,11 +18,12 @@ new joint.dia.Paper({
     })
 });
 
-joint.shapes.standard.Rectangle.define('HyperlinkLabelRectangle', {
+const concrete = joint.shapes.standard.Rectangle.define('Concrete', {
     attrs: {
         body: {
             fill: '#ffffff',
-            stroke: '#000000'
+            stroke: '#000000',
+            strokeWidth: 1
         },
         link: {
             xlinkShow: 'new',
@@ -34,7 +35,6 @@ joint.shapes.standard.Rectangle.define('HyperlinkLabelRectangle', {
         tagName: 'rect',
         selector: 'body',
     }, {
-        // `link` envelops only `label`
         tagName: 'a',
         selector: 'link',
         children: [{
@@ -44,8 +44,25 @@ joint.shapes.standard.Rectangle.define('HyperlinkLabelRectangle', {
     }]
 });
 
-function node(x, y, label, link) {
-    return new joint.shapes.HyperlinkLabelRectangle({
+const abstract = joint.shapes.Concrete.define('Abstract', {
+    attrs: {
+        body: {
+            strokeDasharray: '10,2'
+        }
+    }
+});
+
+const algo = joint.shapes.Concrete.define('Algo', {
+    attrs: {
+        body: {
+            rx: 5,
+            ry: 5,
+        }
+    }
+});
+
+function node(x, y, label, link, shape) {
+    return new shape({
         position: { x: x, y: y },
         // size: { width: 40 + 5*label.length, height: 30 },
         size: { width: 120, height: 30 },
@@ -59,12 +76,12 @@ function node(x, y, label, link) {
 const dx = 210, dy1 = 140, dy2 = dy1/4, dy3 = dy2/1.5;
 
 function right(n) {
-    let box = n.getBBox();
+    const box = n.getBBox();
     return box.x + box.width + dx;
 }
 
 function getY(n) {
-    let box = n.getBBox();
+    const box = n.getBBox();
     return box.y;
 }
 
@@ -72,15 +89,15 @@ function link(n1, n2) {
     new joint.shapes.standard.Link().source(n1).target(n2).addTo(graph);
 }
 
-let linkedlist = node(0, dy1/4, 'Liste chaînée', 'http://en.wikipedia.org/wiki/Array_data_structure');
-let tree = node(0, dy1/4 + dy1, 'Arbre', 'http://www.google.com');
-let array = node(0, dy1/4 + 2*dy1, 'Tableau', 'http://en.wikipedia.org/wiki/Array_data_structure');
+const linkedlist = node(0, dy1/4, 'Liste chaînée', '', concrete);
+const tree = node(0, dy1/4 + dy1, 'Arbre', '', concrete);
+const array = node(0, dy1/4 + 2*dy1, 'Tableau', '', concrete);
 
-let queue = node(dx, getY(linkedlist) - dy2, 'Queue', 'http://en.wikipedia.org/wiki/Queue_(data_structure)');
-let stack = node(dx, getY(linkedlist) + dy2, 'Stack', 'http://en.wikipedia.org/wiki/Queue_(data_structure)');
-let bst = node(dx, getY(tree), 'ABR', 'http://en.wikipedia.org/wiki/Breadth-first_search');
-let heap = node(dx, .5*(getY(array) + getY(tree)), 'Tas', 'http://en.wikipedia.org/wiki/Breadth-first_search');
-let hash = node(dx, getY(array), 'Table de hachage', 'http://en.wikipedia.org/wiki/Breadth-first_search');
+const queue = node(dx, getY(linkedlist) - dy2, 'Queue', '', abstract);
+const stack = node(dx, getY(linkedlist) + dy2, 'Stack', '', abstract);
+const bst = node(dx, getY(tree), 'ABR', '', concrete);
+const heap = node(dx, .5*(getY(array) + getY(tree)), 'Tas', '', concrete);
+const hash = node(dx, getY(array), 'Table de hachage', '', concrete);
 link(linkedlist, queue);
 link(linkedlist, stack);
 link(tree, bst);
@@ -88,11 +105,11 @@ link(tree, heap);
 link(array, heap);
 link(array, hash);
 
-let bfs = node(2*dx, getY(queue), 'BFS', 'http://en.wikipedia.org/wiki/Breadth-first_search');
-let dfs = node(2*dx, getY(stack), 'DFS', 'http://en.wikipedia.org/wiki/Breadth-first_search');
-let huffman = node(2*dx, .5*(getY(stack) + getY(tree)), 'Huffman', 'http://en.wikipedia.org/wiki/Breadth-first_search');
-let priority = node(2*dx, .5*(getY(bst) + getY(heap)), 'File de priorité', 'http://en.wikipedia.org/wiki/Breadth-first_search');
-let dict = node(2*dx, .5*(getY(heap) + getY(hash)), 'Dictionnaire', 'http://en.wikipedia.org/wiki/Breadth-first_search');
+const bfs = node(2*dx, getY(queue), 'BFS', '', concrete);
+const dfs = node(2*dx, getY(stack), 'DFS', '', concrete);
+const huffman = node(2*dx, .5*(getY(stack) + getY(tree)), 'Huffman', '', algo);
+const priority = node(2*dx, .5*(getY(bst) + getY(heap)), 'File de priorité', '', abstract);
+const dict = node(2*dx, .5*(getY(heap) + getY(hash)), 'Dictionnaire', '', abstract);
 link(queue, bfs);
 link(stack, dfs);
 link(stack, huffman);
@@ -102,3 +119,7 @@ link(heap, priority);
 link(bst, dict);
 link(hash, dict);
 
+const dijkstra = node(3*dx, getY(priority), 'Dijkstra', '', algo);
+const boyermoore = node(3*dx, getY(dict), 'Boyer-Moore', '', algo);
+link(priority, dijkstra);
+link(dict, boyermoore);
